717402780c519223ea85e1648c0b56f1890627a0
ticklingtom <mammamiaadi@gmail.com>
2021-06-04 22:30:25 +0700

    initialize repos

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './tot.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://pornhub.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Website Anak ❤️
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
